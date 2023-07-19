5d024a9899852ad9a25a77083a7994ca44590050
michaelpantic <michael.pantic@gmail.com>
2023-07-19 15:02:18 +0200

    Changes merged from platform from last tests, plus debug output

--- waverider/include/waverider/policy_tuning.h
+++ waverider/include/waverider/policy_tuning.h
@@ -3,11 +3,11 @@
 
 namespace waverider {
 struct PolicyTuning {
-  float r = 0.1;
-  float c = 0.1;
-  float eta_rep = 80;   // n
-  float nu_rep = 1;     // v
-  float eta_damp = 14;  // n
+  float r = 1.3;
+  float c = 0.05;
+  float eta_rep = 3.4;   // n
+  float nu_rep = 0.5;     // v
+  float eta_damp = 2.0;  // n
   float nu_damp = 1;    // v
   bool enable_damper = true;
   bool enable_repulsor = true;
