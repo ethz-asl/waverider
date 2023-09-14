ed5bf02b3dd7a8fd48cd8412d8eef9d04b2c998e
Victor Reijgwart <victorreijgwart@gmail.com>
2023-09-14 10:36:58 +0200

    Cache ESDF and log trajectory + distance stats to CSV

--- chomp_ros/app/chomp_node.cc
+++ chomp_ros/app/chomp_node.cc
@@ -1,9 +1,11 @@
+#include <filesystem>
+#include <fstream>
+
 #include <glog/logging.h>
 #include <ros/ros.h>
 #include <visualization_msgs/MarkerArray.h>
 #include <wavemap/data_structure/volumetric/hashed_blocks.h>
 #include <wavemap/data_structure/volumetric/volumetric_data_structure_base.h>
-#include <wavemap/indexing/index_conversions.h>
 #include <wavemap/utils/esdf/collision_utils.h>
 #include <wavemap/utils/esdf/esdf_generator.h>
 #include <wavemap/utils/interpolation_utils.h>
@@ -31,10 +33,20 @@ int main(int argc, char** argv) {
   ros::Publisher trajectory_pub =
       nh.advertise<visualization_msgs::Marker>("trajectory", 10, true);
 
+  // Settings
+  constexpr float kOccupancyThreshold = -0.1f;
+  constexpr float kRobotRadius = 1.f;
+  constexpr float kChompRobotRadiusPadding = 0.5f;
+  std::filesystem::path occupancy_file_path =
+      "/home/victor/data/wavemaps/newer_college_mine_5cm.wvmp";
+  std::filesystem::path logging_dir = "/home/victor/data/waverider/chomp_stats";
+
+  // Create the logging dir if needed
+  std::filesystem::create_directories(logging_dir);
+
   // Load the occupancy map
   wavemap::VolumetricDataStructureBase::Ptr occupancy_map;
-  wavemap::io::fileToMap(
-      "/home/victor/data/wavemaps/newer_college_mine_5cm.wvmp", occupancy_map);
+  wavemap::io::fileToMap(occupancy_file_path, occupancy_map);
   CHECK_NOTNULL(occupancy_map);
 
   // Publish the occupancy map
@@ -51,15 +63,41 @@ int main(int argc, char** argv) {
     return EXIT_FAILURE;
   }
 
-  // Generate the ESDF
-  constexpr float kOccupancyThreshold = -0.1f;
-  constexpr float kMaxDistance = 2.f;
-  const auto esdf =
-      generateEsdf(*hashed_map, kOccupancyThreshold, kMaxDistance);
+  // Load or generate the ESDF
+  wavemap::HashedBlocks::Ptr esdf;
+  const std::filesystem::path esdf_file_path =
+      std::filesystem::path(occupancy_file_path).replace_extension("dwvmp");
+  if (std::filesystem::exists(esdf_file_path)) {
+    // Load the ESDF
+    LOG(INFO) << "Loading ESDF from path: " << esdf_file_path;
+    wavemap::VolumetricDataStructureBase::Ptr esdf_tmp;
+    if (!wavemap::io::fileToMap(esdf_file_path, esdf_tmp)) {
+      LOG(ERROR) << "Could not load ESDF";
+      return EXIT_FAILURE;
+    }
+    esdf = std::dynamic_pointer_cast<wavemap::HashedBlocks>(esdf_tmp);
+    if (!esdf) {
+      LOG(ERROR) << "Loaded ESDF is not of type hashed blocks";
+      return EXIT_FAILURE;
+    }
+  } else {
+    // Generate the ESDF
+    LOG(INFO) << "Generating ESDF";
+    constexpr float kMaxDistance = 2.f;
+    esdf = std::make_shared<wavemap::HashedBlocks>(
+        generateEsdf(*hashed_map, kOccupancyThreshold, kMaxDistance));
+
+    // Save the ESDF
+    LOG(INFO) << "Saving ESDF to path: " << esdf_file_path;
+    if (!wavemap::io::mapToFile(*esdf, esdf_file_path)) {
+      LOG(ERROR) << "Could not save ESDF";
+      return EXIT_FAILURE;
+    }
+  }
 
   // Publish the ESDF
   wavemap_msgs::Map msg;
-  wavemap::convert::mapToRosMsg(esdf, "odom", ros::Time::now(), msg);
+  wavemap::convert::mapToRosMsg(*esdf, "odom", ros::Time::now(), msg);
   esdf_pub.publish(msg);
 
   // Define the ESDF distance getter
@@ -68,28 +106,27 @@ int main(int argc, char** argv) {
     const wavemap::Point3D position = position_d.cast<wavemap::FloatingPoint>();
     if (wavemap::interpolateTrilinear(*occupancy_map, position) <
         kOccupancyThreshold) {
-      return wavemap::interpolateTrilinear(esdf, position);
+      return wavemap::interpolateTrilinear(*esdf, position);
     } else {
       return 0.f;
     }
   };
 
   // Initialize CHOMP
-  constexpr float kSafetyPadding = 0.5f;
-  constexpr float kRobotRadius = 1.f;
   chomp::ChompOptimizer chomp;
   chomp::ChompParameters params;
-  params.map_resolution = esdf.getMinCellWidth();
+  params.map_resolution = esdf->getMinCellWidth();
   params.w_collision = 10.0;
   params.w_smooth = 0.1;
   params.lambda = 100;  // 20.1
   params.max_iter = 100;
-  params.epsilon = kRobotRadius + kSafetyPadding;
+  params.epsilon = kRobotRadius + kChompRobotRadiusPadding;
   params.decrease_step_size = true;
   chomp.setParameters(params);
   chomp.setDistanceFunction(distance_getter);
 
   // Loop forever (for debugging)
+  size_t run_idx = 0u;
   while (true) {
     // Sample random start and goal positions until a
     // collision-free trajectory between them is found
@@ -101,10 +138,10 @@ int main(int argc, char** argv) {
       }
 
       // Get random start and goal positions
-      const auto start =
-          wavemap::getCollisionFreePosition(*occupancy_map, esdf, kRobotRadius);
-      const auto goal =
-          wavemap::getCollisionFreePosition(*occupancy_map, esdf, kRobotRadius);
+      const auto start = wavemap::getCollisionFreePosition(*occupancy_map,
+                                                           *esdf, kRobotRadius);
+      const auto goal = wavemap::getCollisionFreePosition(*occupancy_map, *esdf,
+                                                          kRobotRadius);
       if (!start || !goal) {
         LOG(ERROR) << "Could not find collision free start and goal positions";
         return EXIT_FAILURE;
@@ -149,16 +186,39 @@ int main(int argc, char** argv) {
       chomp.solveProblem(start->cast<double>(), goal->cast<double>(), N,
                          &chomp_output);
 
-      // Check if the trajectory is collision free
+      // Create a log file for the trajectory
+      const auto log_file_path =
+          logging_dir / ("run_" + std::to_string(run_idx));
+      std::ofstream log_file_ostream(
+          log_file_path, std::ofstream::out | std::ofstream::binary);
+      if (!log_file_ostream.is_open()) {
+        LOG(WARNING) << "Could not open file " << log_file_path
+                     << " for writing. Error: " << strerror(errno);
+        return EXIT_FAILURE;
+      }
+
+      // Check the trajectory and log stats
       bool is_collision_free = true;
       for (int idx = 0; idx < chomp_output.trajectory.rows(); ++idx) {
         const auto position = chomp_output.trajectory.row(idx);
         const float esdf_distance = distance_getter(position);
+        // Check if we collided
         if (esdf_distance <= kRobotRadius) {
           is_collision_free = false;
-          break;
         }
+        // Log position and ESDF value to file
+        log_file_ostream << position.x() << ", " << position.y() << ", "
+                         << position.z() << ", " << esdf_distance << "\n";
       }
+
+      // Close the log file and check if writing succeeded
+      log_file_ostream.close();
+      if (!log_file_ostream) {
+        LOG(WARNING) << "Failed to write to file " << log_file_path
+                     << ". Error: " << strerror(errno);
+        return EXIT_FAILURE;
+      }
+
       if (is_collision_free) {
         LOG(INFO) << "Solution trajectory is collision free";
         break;
